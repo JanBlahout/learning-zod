7c747edec01bd614d3fb4f3e57fbc2ac7d34c304
janblahout <jan.blahout@outlook.com>
2024-02-03 14:10:36 +0100

    added schemas.ts

---
+++ src/schemas.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+export const formDataSchema = z
+  .object({
+    username: z.string().min(4, 'Username has to be at least 4 characters'),
+    email: z
+      .string()
+      .email('Please enter a valid email address')
+      .min(1, 'Please enter an email adress'),
+    playGames: z.boolean(),
+    favoriteGame: z.string().optional(),
+  })
+  .superRefine((values, context) => {
+    if (values.playGames === true && values.favoriteGame?.trim().length === 0) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Please provide a favorite game',
+        path: ['favoriteGame'],
+      });
+    }
+  });
